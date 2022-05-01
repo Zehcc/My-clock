79fc85cdebe99e3e753b7cd6355970e200327b0a
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-05-01 12:56:11 +0200

    chore: change countdown date

--- src/components/CountDown/CountDown.jsx
+++ src/components/CountDown/CountDown.jsx
@@ -1,49 +1,46 @@
-import React, { useEffect, useState} from 'react'
-import {BsCalendar3 as CalendarIcon} from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { BsCalendar3 as CalendarIcon } from "react-icons/bs";
 
 const CountDown = () => {
+  const [countDown, setCountDown] = useState();
 
-    const [countDown, setCountDown] = useState();
+  let choosenDate = new Date("Jan 1, 2023 00:00:00").getTime();
 
-    let choosenDate = new Date("May 4, 2022 18:00:00").getTime();
+  useEffect(() => {
+    let interval = setInterval(() => {
+      let now = new Date().getTime();
 
-    useEffect(() => {
+      let secondsLeft = Math.floor(
+        new Date(choosenDate - now).valueOf() / 1000
+      );
 
-        let interval = setInterval(() =>{
-            let now = new Date().getTime();
+      let days = Math.floor(secondsLeft / 86400);
 
-            let secondsLeft = Math.floor(
-                new Date(choosenDate - now).valueOf() / 1000
-            )
+      let hours = Math.floor((secondsLeft % 86400) / 3600);
 
-            let days = Math.floor(secondsLeft / (86400));
+      let minutes = Math.floor((secondsLeft % 3600) / 60);
 
-            let hours = Math.floor((secondsLeft % (86400)) / (3600));
+      let seconds = Math.floor(secondsLeft % 60);
 
-            let minutes = Math.floor((secondsLeft % 3600 ) / 60)
+      setCountDown(`${days} D ${hours} H ${minutes} M ${seconds} S`);
 
-            let seconds = Math.floor((secondsLeft % 60))
+      if (secondsLeft < 0) {
+        clearInterval(interval);
+        setCountDown("SE ACABÓ EL BOOTCAMP 🎉🥳🎉");
+      }
+    }, 1000);
 
-            setCountDown(`${days} D ${hours} H ${minutes} M ${seconds} S`)
-            
-            if(secondsLeft < 0){
-                clearInterval(interval)
-                setCountDown("SE ACABÓ EL BOOTCAMP 🎉🥳🎉")
-            }
-
-        },1000)
-
-        return(() =>{
-            clearInterval(interval)
-        })
-    },[])
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className='countDownContainer'>
-        <CalendarIcon size={60} color ="orange"/>
-        <div className='countDownText'>{countDown}</div>
+      <CalendarIcon size={60} color='orange' />
+      <div className='countDownText'>{countDown}</div>
     </div>
-  )
-}
+  );
+};
 
-export default CountDown
+export default CountDown;
